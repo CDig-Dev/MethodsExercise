0e5cf03c01e4b7b7f4ca700e162706e80c60a0bb
Carlos Digaetano <digaetano.dev@gmail.com>
2025-06-15 20:54:42 -0400

    Completed C# story program excercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //User Input
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal");
+            string animal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string band = Console.ReadLine();
+            
+            //String interpolation
+            Console.WriteLine ($"My name is {name}. My favorite a {color} {animal}. I also enjoy listening to  {band}");
+            
         }
     }
 }
